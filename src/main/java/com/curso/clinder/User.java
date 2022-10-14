2449b43d337c10751bbcba300ea4c235e98c5121
Padilla Montero <dpadilla@t-systems.es>
2022-10-14 17:19:14 +0200

    add set get user

--- src/main/java/com/curso/clinder/User.java
+++ src/main/java/com/curso/clinder/User.java
@@ -4,22 +4,74 @@
  */
 package com.curso.clinder;
 
+import static com.curso.clinder.Constants.*;
+import java.util.ArrayList;
+
 /**
  *
  * @author abzambra
  */
 public class User {
     
-    public String pass;
-    public String name;
-    public int age;
-    public String pleasures;
-    public String sexualOrientation;
+    private String pass;
+    private String name;
+    private int age;
+    private ArrayList<Integer> pleasures;
+    private int sexualOrientation;
+    private int gender;
+    
 
     public User(String pass, String name) {
         this.pass = pass;
         this.name = name;
     }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public ArrayList<Integer> getPleasures() {
+        return pleasures;
+    }
+
+    public void setPleasures(ArrayList<Integer> pleasures) {
+        this.pleasures = pleasures;
+    }
+
+    public int getSexualOrientation() {
+        return sexualOrientation;
+    }
+
+    public void setSexualOrientation(int sexualOrientation) {
+        this.sexualOrientation = sexualOrientation;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        String likes="";
+        for (Integer pleasure : pleasures) {
+            likes+=pleasure.toString()+LISTSEPARE;
+        }
+        
+        String toWrite= pass + SEPARE + name + SEPARE + age + SEPARE + likes + SEPARE + sexualOrientation + SEPARE + gender;
+        
+        
+        return  toWrite;
+    }
+    
+    
     
     
 }
