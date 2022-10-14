d9c1d580245b910f66d686f1cc1d602314e8e9ea
Padilla Montero <dpadilla@t-systems.es>
2022-10-14 15:45:20 +0200

    COnstants prepare, Reader and Writer not working

---
+++ src/main/java/com/curso/clinder/Constants.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.curso.clinder;
+
+/**
+ * class with all the app constants
+ * @author dpadilla
+ */
+public class Constants {
+    public static final String SEPARE="|";
+    public static final String LISTSEPARE="¡";
+}
---
+++ src/main/java/com/curso/clinder/Writer.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.curso.clinder;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ *
+ * @author dpadilla
+ */
+public class Writer {
+    private FileWriter writer;
+    private String fileName;
+
+    public Writer(String file) {
+        this.fileName = file;
+    }
+    
+    public void writeFile(HashMap<String,User> db) throws IOException {
+
+        File file = new File(fileName);
+        writer = new FileWriter(file);
+        String toWrite;
+
+        for (Map.Entry<String, User> entry : db.entrySet()) {
+            String key = entry.getKey();
+            User val = entry.getValue();
+            
+            toWrite = val.toString();
+            writer.write(toWrite);
+        }
+        
+        
+        writer.close();
+
+    }
+}
